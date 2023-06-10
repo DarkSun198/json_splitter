7e006d998b7f79a5ed9ca61eac8f1d4643e734e8
DarkSun198 <91627126+DarkSun198@users.noreply.github.com>
2023-06-10 14:37:21 +0100

    Update remote_loader.sh

--- remote_loader.sh
+++ remote_loader.sh
@@ -30,9 +30,9 @@ mkdir output
 #!/bin/bash
 
 # Read in the list of possible values for X from input.txt
-IFS=$'\n' read -d '' -r -a x_values < input.txt
+#IFS=$'\n' read -d '' -r -a x_values < input.txt
 
-#x_values=("Th,3s,Td" "2c,8d,Jh" "5d,6d,7d")
+x_values=("Th,3s,Td") # "2c,8d,Jh" "5d,6d,7d")
 
 # Loop through each value of X and create a separate file for each one
 for x in "${x_values[@]}"; do
@@ -51,13 +51,13 @@ for x in "${x_values[@]}"; do
     	    echo "No JSON for $x"
 
 	    # Create the file with the desired contents
-	    echo "set_pot 25
+	    echo "set_pot 6
 set_effective_stack 100
 set_board $x
-set_range_ip AA
-set_range_oop AA
-set_bet_sizes oop,flop,bet,100
-set_bet_sizes oop,flop,raise,100
+set_range_ip AA,AKs,AQs,AJs,ATs,A9s,A8s,A7s,A6s,A5s,A4s,A3s,A2s,AKo,KK,KQs,KJs,KTs,K9s,K8s,K7s,K6s,K5s,K4s,K3s,K2s,AQo,KQo,QQ,QJs,QTs,Q9s,Q8s,Q7s,Q6s,Q5s,Q4s,Q3s,Q2s,AJo,KJo,QJo,JJ,JTs,J9s,J8s,J7s,J6s,J5s,J4s,ATo,KTo,QTo,JTo,TT,T9s,T8s,T7s,T6s,T5s,A9o,K9o,Q9o,J9o,T9o,99,98s,97s,96s,95s,A8o,K8o,Q8o,J8o,T8o,98o,88,87s,86s,85s,A7o,K7o,Q7o,77,76s,75s,A6o,K6o,66,65s,A5o,K5o,55,54s,A4o,K4o,44,A3o,33,A2o,22
+set_range_oop AA,AKs,AQs,AJs,ATs,A9s,A8s,A7s,A6s,A5s,A4s,A3s,A2s,AKo,KK,KQs,KJs,KTs,K9s,K8s,K7s,K6s,K5s,K4s,K3s,K2s,AQo,KQo,QQ,QJs,QTs,Q9s,Q8s,Q7s,Q6s,Q5s,Q4s,Q3s,Q2s,AJo,KJo,QJo,JJ,JTs,J9s,J8s,J7s,J6s,J5s,J4s,ATo,KTo,QTo,JTo,TT,T9s,T8s,T7s,T6s,T5s,A9o,K9o,Q9o,J9o,T9o,99,98s,97s,96s,95s,A8o,K8o,Q8o,J8o,T8o,98o,88,87s,86s,85s,A7o,K7o,Q7o,77,76s,75s,A6o,K6o,66,65s,A5o,K5o,55,54s,A4o,K4o,44,A3o,33,A2o,22
+set_bet_sizes oop,flop,bet,50 100
+set_bet_sizes oop,flop,raise,50 100
 set_bet_sizes ip,flop,bet,100
 set_bet_sizes ip,flop,raise,100
 set_bet_sizes oop,turn,bet,100
@@ -67,12 +67,14 @@ set_bet_sizes ip,turn,raise,100
 set_bet_sizes oop,river,bet,100
 set_bet_sizes oop,river,donk,100
 set_bet_sizes oop,river,raise,100
+set_bet_sizes oop,river,allin
 set_bet_sizes ip,river,bet,100
 set_bet_sizes ip,river,raise,100
+set_bet_sizes ip,river,allin
 set_allin_threshold 0.8
 build_tree
-set_thread_num 2
-set_accuracy 5.0
+set_thread_num 32
+set_accuracy 0.5
 set_max_iteration 101
 set_print_interval 10
 set_use_isomorphism 1
