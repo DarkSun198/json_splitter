1d84ba42c12216cef92fafa52309dc8fd90fcefd
DarkSun198 <91627126+DarkSun198@users.noreply.github.com>
2023-06-10 14:43:28 +0100

    Update remote_loader.sh

--- remote_loader.sh
+++ remote_loader.sh
@@ -56,10 +56,10 @@ set_effective_stack 100
 set_board $x
 set_range_ip AA,AKs,AQs,AJs,ATs,A9s,A8s,A7s,A6s,A5s,A4s,A3s,A2s,AKo,KK,KQs,KJs,KTs,K9s,K8s,K7s,K6s,K5s,K4s,K3s,K2s,AQo,KQo,QQ,QJs,QTs,Q9s,Q8s,Q7s,Q6s,Q5s,Q4s,Q3s,Q2s,AJo,KJo,QJo,JJ,JTs,J9s,J8s,J7s,J6s,J5s,J4s,ATo,KTo,QTo,JTo,TT,T9s,T8s,T7s,T6s,T5s,A9o,K9o,Q9o,J9o,T9o,99,98s,97s,96s,95s,A8o,K8o,Q8o,J8o,T8o,98o,88,87s,86s,85s,A7o,K7o,Q7o,77,76s,75s,A6o,K6o,66,65s,A5o,K5o,55,54s,A4o,K4o,44,A3o,33,A2o,22
 set_range_oop AA,AKs,AQs,AJs,ATs,A9s,A8s,A7s,A6s,A5s,A4s,A3s,A2s,AKo,KK,KQs,KJs,KTs,K9s,K8s,K7s,K6s,K5s,K4s,K3s,K2s,AQo,KQo,QQ,QJs,QTs,Q9s,Q8s,Q7s,Q6s,Q5s,Q4s,Q3s,Q2s,AJo,KJo,QJo,JJ,JTs,J9s,J8s,J7s,J6s,J5s,J4s,ATo,KTo,QTo,JTo,TT,T9s,T8s,T7s,T6s,T5s,A9o,K9o,Q9o,J9o,T9o,99,98s,97s,96s,95s,A8o,K8o,Q8o,J8o,T8o,98o,88,87s,86s,85s,A7o,K7o,Q7o,77,76s,75s,A6o,K6o,66,65s,A5o,K5o,55,54s,A4o,K4o,44,A3o,33,A2o,22
-set_bet_sizes oop,flop,bet,50 100
-set_bet_sizes oop,flop,raise,50 100
-set_bet_sizes ip,flop,bet,100
-set_bet_sizes ip,flop,raise,100
+set_bet_sizes oop,flop,bet,50,100
+set_bet_sizes oop,flop,raise,50,100
+set_bet_sizes ip,flop,bet,50,100
+set_bet_sizes ip,flop,raise,50,100
 set_bet_sizes oop,turn,bet,100
 set_bet_sizes oop,turn,raise,100
 set_bet_sizes ip,turn,bet,100
