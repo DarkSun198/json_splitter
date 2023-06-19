259b150eddbeddcfc739e59cda80a81286399582
DarkSun198 <91627126+DarkSun198@users.noreply.github.com>
2023-06-19 21:31:19 +0100

    Update remote_loader.sh

--- remote_loader.sh
+++ remote_loader.sh
@@ -67,7 +67,7 @@ set_bet_sizes ip,turn,raise,100
 set_allin_threshold 0.8
 build_tree
 set_thread_num 16
-set_accuracy 0.5
+set_accuracy 1.0
 set_max_iteration 201
 set_print_interval 10
 set_use_isomorphism 1
